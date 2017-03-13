ab0a765a02439b4343e9e7b69307e3a55c01901a
KamranG12 <kamran.jabiyev@aplusa-security.com>
2017-03-13 16:43:51 +0400

    last

--- accordion/script.js
+++ accordion/script.js
@@ -1,12 +1,25 @@
-var acc = document.getElementsByClassName("accordion");
+function accordion1() {
+document.getElementsByClassName("panel")[0].style.display ='block';
+document.getElementsByClassName("icon")[0].innerHTML = '<i class="fa fa-caret-down" aria-hidden="true"></i>';
+document.getElementsByClassName("panel")[1].style.display ='none';
+document.getElementsByClassName("icon")[1].innerHTML = '<i class="fa fa-caret-right left" aria-hidden="true"></i>';
+document.getElementsByClassName("panel")[2].style.display ='none';
+document.getElementsByClassName("icon")[2].innerHTML = '<i class="fa fa-caret-right left" aria-hidden="true"></i>';
+}
 
-for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    }
+function accordion2() {
+document.getElementsByClassName("panel")[0].style.display ='none';
+document.getElementsByClassName("icon")[0].innerHTML = '<i class="fa fa-caret-right left" aria-hidden="true"></i>';
+document.getElementsByClassName("panel")[1].style.display ='block';
+document.getElementsByClassName("icon")[1].innerHTML = '<i class="fa fa-caret-down" aria-hidden="true"></i>';
+document.getElementsByClassName("panel")[2].style.display ='none';
+document.getElementsByClassName("icon")[2].innerHTML = '<i class="fa fa-caret-right left" aria-hidden="true"></i>';
+}
+function accordion3() {
+document.getElementsByClassName("panel")[0].style.display ='none';
+document.getElementsByClassName("icon")[0].innerHTML = '<i class="fa fa-caret-right left" aria-hidden="true"></i>';
+document.getElementsByClassName("panel")[1].style.display ='none';
+document.getElementsByClassName("icon")[1].innerHTML = '<i class="fa fa-caret-right left" aria-hidden="true"></i>';
+document.getElementsByClassName("panel")[2].style.display ='block';
+document.getElementsByClassName("icon")[2].innerHTML = '<i class="fa fa-caret-down" aria-hidden="true"></i>';
 }
